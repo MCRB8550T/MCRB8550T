13fa3755812d5a4b43615a109eacdf5ec54a569e
MCRB8550T <84543533+MCRB8550T@users.noreply.github.com>
2021-11-20 10:46:23 -0600

    Update tabla_diferencias_divididas.py

--- tabla_diferencias_divididas.py
+++ tabla_diferencias_divididas.py
@@ -1,24 +1,43 @@
-import numpy as np
+matriz triangular inferior para la tabla de diferencias divididas
 
-def divided_diff(x, y):
-     n = len(y)
-     coef = np.zeros([n, n])
-     coef[:,0]
-     
-     for j in range(1, n):
-         for i in range(n-j):
-              coef[i][j] = (coef[i+1][j-1]-coef[i][j-1])
-              
-     return coef 
-     
- def poly(coef, xd, x):
-      n = len(xd)
-      p = coef[n]
-      for k in range(1, n+1):
-           p = coef[n-k]+(x-xd[n-k])*p
-      return p 
-      
- x_new = np.arange(-5, 2.1, .1)
- y_new = newton_poly(a_s, x, x_new)
- 
- print([x_new[i]+''+y_new[i] for i in range(0, len(x_new))])
+import numpy as np
+import matplotlib.pyplot as plt
+
+from copy import deepcopy
+
+
+
+def dd(x, i, j):
+  return x[i+j]-x[i]
+
+'''
+la matriz triangular inferior para la table de diferencias dividas
+'''
+def matriz_ti(x, y):
+  n = len(x)
+  rows = [["0" for i in range(0, n)] for i in range(0, n)]
+  ri = -1
+  for row in rows:
+    print("ligne a nouveau \n")
+    ci=-1
+    ri += 1
+    for col in row:
+      print("colone a nouveau... \n")
+      ci +=1
+      if ci==0:
+        print("zeroth column:")
+        rows[ri][ci]="1"
+        print(rows[ri][ci])
+      elif(ci<=ri):
+        print("le nombre de colone est moins o egal!")
+        prod = ""
+        for k in range(0, ci):
+          prod = prod + "(x_"+str(ci)+"-x_"+str(k)+")"
+        print(str(ri)+","+str(ci))
+        print(prod)
+        rows[ri][ci]=prod
+      else:
+        print("de lo contrario, caso alcanzado!")
+        print(str(ri)+","+str(ci)+"(2)")
+        print(rows[ri][ci])
+     return rows
